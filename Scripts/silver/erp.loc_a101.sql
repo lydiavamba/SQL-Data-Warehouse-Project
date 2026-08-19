8f4e53d6d7fa375485f71a4cf43c3191566bd294
lydiavamba <lydiahvamba@gmail.com>
2026-08-19 09:21:15 +0300

    Create SQL script for data standardization and insertion
    This SQL script standardizes country data and inserts it into the erp_loc_a101 table while ensuring consistency with crm_cust_info.

---
+++ Scripts/silver/erp.loc_a101.sql
@@ -0,0 +1,21 @@
+-- -- Data Standardization & Consistency
+SELECT DISTINCT cntry
+FROM silver.erp_loc_a101;
+
+
+-- Comparing if the data match erp_loc and crm_cust
+INSERT INTO silver.erp_loc_a101
+(cid,cntry)
+SELECT
+    REPLACE(cid, '-', '') AS cid,
+    CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+         WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+         WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+         ELSE TRIM(cntry)
+    END AS cntry
+FROM bronze.erp_loc_a101
+WHERE REPLACE(cid, '-', '') NOT IN ('CID');
+
+
+WHERE REPLACE (cid,'-','')NOT IN
+(SELECT cst_key FROM silver.crm_cust_info)
